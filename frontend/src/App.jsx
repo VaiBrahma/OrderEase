378e1191617d2aa2da3c2103185e10edf0b56062
Vaibhav Singh <119793533+DiloKaaShooter@users.noreply.github.com>
2024-06-22 20:41:25 +0530

    menuItem created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,13 @@ import Admin from './pages/Admin/Admin';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import NotFound from './components/NotFound';
 import RestaurentMenu from './pages/User/RestaurentMenu';
+import MenuItem from './components/MenuItem/MenuItem';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route  path="/" element={<Home />}>
-        <Route path="/user" element={<User />} />
+        <Route path="/user" element={<MenuItem />} />
         <Route path="/user/:restaurentId" element={<RestaurentMenu />} />
         <Route path="/admin" element={<Admin />} />
       </Route>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,10 +2,10 @@ import { Outlet } from "react-router-dom"
 
 const Home = () => {
   return (
-    <>
-    <div>Welcome to our site</div>
-    <Outlet/>
-    </>
+    <div style={{background: `url('/images/background.jpg') no-repeat center center/cover`, height: '100vh', width:'100vw'}}>
+        <div>Welcome to our site</div>
+        <Outlet/>
+    </div>
   )
 }
 
