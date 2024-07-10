7166afcfd8d65d46c60ab8856e28800aea0603c8
Vaibhav Singh <119793533+DiloKaaShooter@users.noreply.github.com>
2024-07-11 01:10:31 +0530

    vite.config.js is not supported by vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
       "/socket.io": {
         target: "http://localhost:5000",
         ws: true,
